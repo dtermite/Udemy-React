1af7bfb5ca0a3310333464fe3e0303582cb85a35
Diego Termite <diegotermite@gmail.com>
2022-11-03 19:04:51 -0300

    terminado clase de const y let

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+// variables y constantes
+const nombre='Diego';
+const apellido='Termite'
+
+let valorDado=5;
+valorDado=4;
+console.log(nombre,apellido,valorDado);
+
+if (true){
+    let valorDado=6;
+    console.log(valorDado);
+}
+
+console.log(valorDado);
