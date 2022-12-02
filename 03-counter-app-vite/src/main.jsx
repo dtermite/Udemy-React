ba9908ef04e7bca68202ae3e824a9bcc0c0d5579
Diego Termite <diegotermite@gmail.com>
2022-12-02 17:14:27 -0300

    primer Hola mundo con Vite

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+//Estos son los 2 elementos basicos para renderizar la App
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//
 
+function App(){
+    return (<h1>Hola Mundo</h1>);
+}
+
+//renderizo el elemente con id 'root' del index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    // React siempre se ejecuta en StrictMode
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
