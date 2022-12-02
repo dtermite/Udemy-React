b53ffa4d711dd933d66391eb33afc03bd02f3e2c
Diego Termite <diegotermite@gmail.com>
2022-12-02 17:42:15 -0300

    primer componente

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+/*export function App(){
+    return (<h1>Hola Mundo!!!</h1>);
+}
+
+export const hola='123';
+*/
+
+//Creado con shortcut rafc
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,16 +1,15 @@
 //Estos son los 2 elementos basicos para renderizar la App
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
 //
 
-function App(){
-    return (<h1>Hola Mundo</h1>);
-}
+
 
 //renderizo el elemente con id 'root' del index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
     // React siempre se ejecuta en StrictMode
     <React.StrictMode>
-        <App />
+        <HelloWorldApp />
     </React.StrictMode>
 );
