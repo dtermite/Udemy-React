a6e2b129088b3e58d14a5b7d07e5f42130f20cdc
Diego Termite <diegotermite@gmail.com>
2022-12-14 16:12:33 -0300

    comenzado con jest

---
+++ 03-counter-app-vite/src/tests/demo.test.js
@@ -0,0 +1,19 @@
+
+describe('Pruebas en <DemoComponent />', () => {  
+
+    test('esta prueba no puede fallar',()=>{
+    
+        //1.Inicializacion
+        const message1= 'Hola Mundo';
+
+        //2.estimulo    
+        const message2=message1.trim();
+
+        //3.observar el comportamiento esperado
+        expect(message1).toBe(message2);
+    
+});
+
+});
+
+
