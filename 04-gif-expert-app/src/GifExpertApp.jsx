cfcb95f62eb4e3bdb867dee1ba673cc41aad19e7
Diego Termite <diegotermite@gmail.com>
2022-12-21 13:01:57 -0300

    categories

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,37 @@
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One punch', 'Dragon Ball']);
+
+  const onAddCategory= () =>{
+
+  }
+
+  console.log(categories);
+
   return (
     <>
+      {/*Titulo */}
         <h1>GifExpertApp</h1>
+        
+      {/*Imput */}
+
+
+      {/*Listado de Gif */}
+        {/* Gif item */}
+        <button>Agregar</button>
+        <ol>
+          {
+            categories.map(category=>{
+              return <li key={category}>{category}</li>
+
+            })
+          }
+            
+          
+          
+        </ol>
     </>
   )
 }
